c93c843cae12b01f8780f04924c7310fc2cf0c0b
BenjaminS <97973081+benjisho@users.noreply.github.com>
2023-06-07 18:39:57 +0300

    Create install_minikube_ubuntu.sh

---
+++ scripts/install_minikube_ubuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update package lists
+echo "Updating package lists..."
+sudo apt-get update -y
+
+# Install VirtualBox
+echo "Installing VirtualBox..."
+sudo apt-get install -y virtualbox virtualbox-ext-pack
+
+# Download Minikube
+echo "Downloading Minikube..."
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+
+# Move Minikube executable to your PATH
+echo "Moving Minikube to /usr/local/bin/..."
+sudo mv minikube /usr/local/bin/
+
+# Start Minikube
+echo "Starting Minikube..."
+minikube start
+
+# Confirm Installation
+echo "Minikube installation is complete. Run 'minikube status' to confirm installation."
